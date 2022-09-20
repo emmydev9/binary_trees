bd57f902787ba036750079d93cc8c73512fbf06a
Emmy <softdevcoder@gmail.com>
2022-09-20 07:42:22 -0400

    added a betty fix

--- binary_trees.h
+++ binary_trees.h
@@ -46,4 +46,6 @@ typedef  struct queue queue;
 void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
 #endif
