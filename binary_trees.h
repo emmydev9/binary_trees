98d46f4e1ea23f6d98556e18ce28da4806897cca
Emmy <softdevcoder@gmail.com>
2022-09-20 07:31:29 -0400

    added a betty style fix

--- binary_trees.h
+++ binary_trees.h
@@ -6,19 +6,19 @@
 #include <stddef.h>
 
 /**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
+* struct binary_tree_s - Binary tree node
+*
+* @n: Integer stored in the node
+* @parent: Pointer to the parent node
+* @left: Pointer to the left child node
+* @right: Pointer to the right child node
+*/
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+int n;
+struct binary_tree_s *parent;
+struct binary_tree_s *left;
+struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -30,15 +30,15 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 /**
- * struct queue - simple queue
- *
- * @node: is the value
- * @next: is the next node in the queue
- */
+* struct queue - simple queue
+*
+* @node: is the value
+* @next: is the next node in the queue
+*/
 struct queue
 {
-	binary_tree_t *node;
-	struct queue *next;
+binary_tree_t *node;
+struct queue *next;
 };
 
 typedef  struct queue queue;
