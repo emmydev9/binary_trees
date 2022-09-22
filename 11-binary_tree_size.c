91d6853ad99a9f548f540056e760921cd4a4af40
Emmy <softdevcoder@gmail.com>
2022-09-22 08:35:28 -0400

    added a betty style fix

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,18 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * 
+ * binary_tree_size - function that measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure
+ * Return: size of binary tree.
  */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-        size_t size = 0;
+		size_t size = 0;
 
-        if (!tree)
-                return (0);
-        if (!tree->left && !tree->right)
-                return (1);
+		if (!tree)
+			return (0);
+		if (!tree->left && !tree->right)
+			return (1);
 
-        size += binary_tree_size(tree->left);
-        size += binary_tree_size(tree->right);
-        return (size + 1);
+		size += binary_tree_size(tree->left);
+		size += binary_tree_size(tree->right);
+		return (size + 1);
 }
